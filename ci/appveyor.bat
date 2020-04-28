fde479b089d385c9d9bc3be8766fdee66a1dce54
Victor <vicr12345@gmail.com>
2020-04-28 23:32:10 +1000

    Hopefully get macOS and Windows builds going?

---
+++ ci/appveyor.bat
@@ -0,0 +1,46 @@
+if "%APPVEYOR_REPO_TAG_NAME%"=="continuous" (
+
+    exit 1
+
+)
+
+git submodule init
+git submodule update
+
+set QTDIR=C:\Qt\5.12\msvc2017_64
+set PATH=%PATH%;%QTDIR%\bin
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+git clone https://github.com/vicr123/the-libs.git
+cd the-libs
+git checkout blueprint
+qmake the-libs.pro "CONFIG+=release"
+nmake release
+nmake install
+cd ..
+
+git clone https://github.com/vicr123/contemporary-theme.git
+cd contemporary-theme
+qmake Contemporary.pro "CONFIG+=release"
+nmake release
+cd ..
+
+mkdir build
+cd build
+qmake ../theFrame.pro "CONFIG+=release"
+nmake release
+cd ..
+
+mkdir deploy
+mkdir deploy\styles
+mkdir deploy\translations
+mkdir deploy\audio
+copy "contemporary-theme\release\Contemporary.dll" deploy\styles
+copy build\frame\release\theFrame.exe deploy
+copy translations\*.qm deploy\translations
+copy audio\* deploy\audio
+copy build\libtheframe\release\libtheframe.dll deploy
+copy build\theframe-render\release\theframe-render.exe deploy
+copy "C:\Program Files\thelibs\lib\the-libs.dll" deploy
+cd deploy
+windeployqt theFrame.exe -svg -multimedia
--- ci/extra-steps-mac.sh
+++ ci/extra-steps-mac.sh
@@ -1,13 +1,18 @@
 cd build
 mv frame/theFrame.app ./
 
+ls
+ls theFrame.app
+ls theFrame.app/Contents
+ls theFrame.app/Contents/MacOS
+
 LIBTHEFRAME_FRAMEWORK_RELATIVE_PATH='libtheframe.framework/Versions/1/libtheframe'
 
 # Install the render process
 cp theframe-render/theframe-render theFrame.app/Contents/MacOS/
 
 # Install the libtheframe framework
-mkdir frame/theFrame.app/Contents/Frameworks
+mkdir theFrame.app/Contents/Frameworks
 cp -r libtheframe/libtheframe.framework theFrame.app/Contents/Frameworks
 install_name_tool -change $LIBTHEFRAME_FRAMEWORK_RELATIVE_PATH @executable_path/../Frameworks/$LIBTHEFRAME_FRAMEWORK_RELATIVE_PATH theFrame.app/Contents/MacOS/theFrame
 install_name_tool -change $LIBTHEFRAME_FRAMEWORK_RELATIVE_PATH @executable_path/../Frameworks/$LIBTHEFRAME_FRAMEWORK_RELATIVE_PATH theFrame.app/Contents/MacOS/theframe-render
