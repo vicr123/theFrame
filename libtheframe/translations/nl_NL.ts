5481e9d75e1818029d308df67c893b9d74a62c34
Mart Koster <the947thtokay@codingtime.nl>
2020-05-25 14:18:14 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: theFrame/theFrame Core
    Translate-URL: https://translate.vicr123.com/projects/theframe/theframe-core/nl/

--- libtheframe/translations/nl_NL.ts
+++ libtheframe/translations/nl_NL.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../elements/element.cpp" line="67"/>
         <source>New Element</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw Element</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../elements/ellipseelement.cpp" line="60"/>
         <source>Geometry</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometrie</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="62"/>
         <source>Background Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Achtergrondkleur</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="64"/>
         <source>Border Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Randkleur</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="66"/>
         <source>Border Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Randbreedte</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="68"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Ondoorzichtigheid</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="91"/>
         <source>Ellipse</source>
-        <translation type="unfinished"></translation>
+        <translation>Ellips</translation>
     </message>
 </context>
 <context>
@@ -47,17 +47,17 @@
     <message>
         <location filename="../elements/groupelement.cpp" line="56"/>
         <source>Offset</source>
-        <translation type="unfinished"></translation>
+        <translation>Afwijking</translation>
     </message>
     <message>
         <location filename="../elements/groupelement.cpp" line="58"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Ondoorzichtigheid</translation>
     </message>
     <message>
         <location filename="../elements/groupelement.cpp" line="73"/>
         <source>Group</source>
-        <translation type="unfinished"></translation>
+        <translation>Groep</translation>
     </message>
 </context>
 <context>
@@ -65,27 +65,27 @@
     <message>
         <location filename="../elements/pictureelement.cpp" line="93"/>
         <source>Geometry</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometrie</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="95"/>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Bron</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="97"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Ondoorzichtigheid</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="117"/>
         <source>All Images (*.png *.jpg *.jpeg *.gif *.svg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle Afbeeldingen (*.png *.jpg *.jpeg *.gif *.svg)</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="124"/>
         <source>Picture</source>
-        <translation type="unfinished"></translation>
+        <translation>Afbeelding</translation>
     </message>
 </context>
 <context>
@@ -93,32 +93,32 @@
     <message>
         <location filename="../elements/rectangleelement.cpp" line="81"/>
         <source>Rectangle</source>
-        <translation type="unfinished"></translation>
+        <translation>Rechthoek</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="86"/>
         <source>Geometry</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometrie</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="88"/>
         <source>Background Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Achtergrondkleur</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="90"/>
         <source>Border Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Randkleur</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="92"/>
         <source>Border Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Randbreedte</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="94"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Ondoorzichtigheid</translation>
     </message>
 </context>
 <context>
@@ -126,43 +126,43 @@
     <message>
         <location filename="../elements/textelement.cpp" line="25"/>
         <source>Edit Me!</source>
-        <translation type="unfinished"></translation>
+        <translation>Bewerk Mij!</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="118"/>
         <location filename="../elements/textelement.cpp" line="155"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekst</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="120"/>
         <source>Position</source>
-        <translation type="unfinished"></translation>
+        <translation>Positie</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="122"/>
         <source>Anchor Point</source>
-        <translation type="unfinished"></translation>
+        <translation>Ankerpunt</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="124"/>
         <source>Font Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekstgrootte</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="126"/>
         <source>Text Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekstkleur</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="128"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Lettertype</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="130"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Ondoorzichtigheid</translation>
     </message>
 </context>
 <context>
@@ -170,7 +170,7 @@
     <message>
         <location filename="../elements/viewportelement.cpp" line="87"/>
         <source>Canvas</source>
-        <translation type="unfinished"></translation>
+        <translation>Canvas</translation>
     </message>
 </context>
 </TS>
