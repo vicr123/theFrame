76968b8c9ebc2289c1e8ab586846c2a0276ee086
Atahan Reis <atahanreis@icloud.com>
2020-12-25 17:24:12 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: theFrame/theFrame Core
    Translate-URL: https://translate.vicr123.com/projects/theframe/theframe-core/en_GB/

--- libtheframe/translations/en_GB.ts
+++ libtheframe/translations/en_GB.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>Element</name>
     <message>
         <location filename="../elements/element.cpp" line="67"/>
         <source>New Element</source>
-        <translation type="unfinished"></translation>
+        <translation>New Element</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../elements/ellipseelement.cpp" line="60"/>
         <source>Geometry</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometry</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="62"/>
         <source>Background Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Background Colour</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="64"/>
         <source>Border Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Border Color</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="66"/>
         <source>Border Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Border Width</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="68"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacity</translation>
     </message>
     <message>
         <location filename="../elements/ellipseelement.cpp" line="91"/>
         <source>Ellipse</source>
-        <translation type="unfinished"></translation>
+        <translation>Ellipse</translation>
     </message>
 </context>
 <context>
@@ -47,17 +47,17 @@
     <message>
         <location filename="../elements/groupelement.cpp" line="56"/>
         <source>Offset</source>
-        <translation type="unfinished"></translation>
+        <translation>Offset</translation>
     </message>
     <message>
         <location filename="../elements/groupelement.cpp" line="58"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacity</translation>
     </message>
     <message>
         <location filename="../elements/groupelement.cpp" line="73"/>
         <source>Group</source>
-        <translation type="unfinished"></translation>
+        <translation>Group</translation>
     </message>
 </context>
 <context>
@@ -65,27 +65,27 @@
     <message>
         <location filename="../elements/pictureelement.cpp" line="93"/>
         <source>Geometry</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometry</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="95"/>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Source</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="97"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacity</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="117"/>
         <source>All Images (*.png *.jpg *.jpeg *.gif *.svg)</source>
-        <translation type="unfinished"></translation>
+        <translation>All Images (*.png *.jpg *.jpeg *.gif *.svg)</translation>
     </message>
     <message>
         <location filename="../elements/pictureelement.cpp" line="124"/>
         <source>Picture</source>
-        <translation type="unfinished"></translation>
+        <translation>Picture</translation>
     </message>
 </context>
 <context>
@@ -93,32 +93,32 @@
     <message>
         <location filename="../elements/rectangleelement.cpp" line="81"/>
         <source>Rectangle</source>
-        <translation type="unfinished"></translation>
+        <translation>Rectangle</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="86"/>
         <source>Geometry</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometry</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="88"/>
         <source>Background Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Background Color</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="90"/>
         <source>Border Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Border Color</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="92"/>
         <source>Border Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Border Width</translation>
     </message>
     <message>
         <location filename="../elements/rectangleelement.cpp" line="94"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacity</translation>
     </message>
 </context>
 <context>
@@ -126,43 +126,43 @@
     <message>
         <location filename="../elements/textelement.cpp" line="25"/>
         <source>Edit Me!</source>
-        <translation type="unfinished"></translation>
+        <translation>Edit Me!</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="118"/>
         <location filename="../elements/textelement.cpp" line="155"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Text</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="120"/>
         <source>Position</source>
-        <translation type="unfinished"></translation>
+        <translation>Position</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="122"/>
         <source>Anchor Point</source>
-        <translation type="unfinished"></translation>
+        <translation>Anchor Point</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="124"/>
         <source>Font Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Font Size</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="126"/>
         <source>Text Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Text Colour</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="128"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font</translation>
     </message>
     <message>
         <location filename="../elements/textelement.cpp" line="130"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacity</translation>
     </message>
 </context>
 <context>
@@ -170,7 +170,7 @@
     <message>
         <location filename="../elements/viewportelement.cpp" line="87"/>
         <source>Canvas</source>
-        <translation type="unfinished"></translation>
+        <translation>Canvas</translation>
     </message>
 </context>
 </TS>
